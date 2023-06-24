147e1ce3a38b45cdd093fc5f0528fda123664413
analoaiza <analoaiza252211@gmail.com>
2023-06-24 11:57:42 -0500

    index html

--- Root/index.js
+++ Root/index.js
@@ -1,14 +1,35 @@
 let button = document.querySelector(".button")
 let RickyMorti= "https://rickandmortyapi.com/api/character";
-let RickyFunction= async()=>{
+let list = document.querySelector(".list");
+
+let getCharacters =async()=>{
     let res = await fetch(RickyMorti)
     let json = await res.json()
-    console.log(json)
+    return json;
+
 }
 
-button.addEventListener("click", ()=>{
-  
-    RickyFunction()
-})
+const createCards=(characters)=>{ 
+    let html =""
+    for (let i=0; i<characters.results.length; i++) {
+    html = `<div class='card'><img/ src=${characters.results[i].image}><h3>${characters.results[i].name}</h3><p>${characters.results[i].status}</p></div>`
+    printCards(html)
+}
 
+    return html
+}
 
+ (async()=>{
+    const characters = await getCharacters()
+    console.log(createCards(characters))
+    //list.appendChild(displayCards(characters));
+    //console.log(html);
+ })()
+
+ const printCards=(card)=>{
+    console.log(card);
+    let listElement=document.createElement('li');
+    listElement.innerHTML=card;
+    list.append(listElement);
+
+ }
