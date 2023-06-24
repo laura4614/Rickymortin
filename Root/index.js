73f3b5a2382b51d6bb47dd922a3564bfd948c72e
luiri96 <luirivera96@gmail.com>
2023-06-24 10:03:10 -0500

    add js to html page

--- Root/index.js
+++ Root/index.js
@@ -1,9 +1,14 @@
 let button = document.querySelector(".boton")
 let RickyMorti= "https://rickandmortyapi.com/api/character";
+let RickyFunction= async()=>{
 let res = await fetch(RickyMorti)
 let json = await res.json()
-
-button.onclick = function (){ 
-    console.log(res)
-
+console.log(json)
 }
+
+document.addEventListener("click", ()=>{
+  
+    RickyFunction()
+})
+
+console.log("Hola")
