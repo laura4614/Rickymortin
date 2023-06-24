5b3de4fafc48096e693abc0c35d1f93782521d29
luiri96 <luirivera96@gmail.com>
2023-06-24 10:17:16 -0500

    fix buttom functionality

--- Root/index.js
+++ Root/index.js
@@ -1,14 +1,14 @@
-let button = document.querySelector(".boton")
+let button = document.querySelector(".button")
 let RickyMorti= "https://rickandmortyapi.com/api/character";
 let RickyFunction= async()=>{
-let res = await fetch(RickyMorti)
-let json = await res.json()
-console.log(json)
+    let res = await fetch(RickyMorti)
+    let json = await res.json()
+    console.log(json)
 }
 
-document.addEventListener("click", ()=>{
+button.addEventListener("click", ()=>{
   
     RickyFunction()
 })
 
-console.log("Hola")
+
